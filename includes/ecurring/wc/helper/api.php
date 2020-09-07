b0e2158fc5b78783d9d0abb5ebcde38492b6af2f
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2020-09-07 18:12:23 +0300

    update comments

--- includes/ecurring/wc/helper/api.php
+++ includes/ecurring/wc/helper/api.php
@@ -14,22 +14,38 @@ class eCurring_WC_Helper_Api
 		$this->settings_helper = $settings_helper;
 	}
 
+    /**
+     * Send request for creating new eCurring subscription.
+     *
+     * @param array $data
+     *
+     * @return string API response body or error message.
+     */
 	public function createSubscription(array $data)
     {
         return $this->apiCall( 'POST', 'https://api.ecurring.com/subscriptions', $data );
     }
 
+    /**
+     * Get subscription data using subscription id.
+     *
+     * @param string $subscription_id
+     *
+     * @return string Response body with subscription data or error message.
+     */
     public function getSubscriptionById($subscription_id)
     {
         return $this->apiCall('GET','https://api.ecurring.com/subscriptions/'.$subscription_id);
     }
 
 	/**
-	 * @param $method
-	 * @param $url
-	 * @param bool|array $data
+     * Make eCurring API request call.
+     *
+	 * @param string $method HTTP Method, one of the GET, POST, PATH, DELETE.
+	 * @param string $url Request target URL.
+	 * @param bool|array $data Content to be sent in JSON-encoded format as request body.
 	 *
-	 * @return mixed
+	 * @return string Response body or error message string.
 	 */
 	public function apiCall( $method, $url, $data = false ) {
 
