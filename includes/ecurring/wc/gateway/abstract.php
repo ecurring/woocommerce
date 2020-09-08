0831e03b5a8b3aea93e98a302f407e111e1dafd0
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2020-09-08 10:42:21 +0300

    move subscription exists check to separate method

--- includes/ecurring/wc/gateway/abstract.php
+++ includes/ecurring/wc/gateway/abstract.php
@@ -291,18 +291,7 @@ abstract class eCurring_WC_Gateway_Abstract extends WC_Payment_Gateway
 
             $subscription_id = get_post_meta($order_id, '_ecurring_subscription_id', true);
 
-            $subscription_exists = false;
-
-            if($subscription_id) {
-                $subscription_data = $api->getSubscriptionById($subscription_id);
-
-                if(isset($subscription_data['data']['type']) && $subscription_data['data']['type'] === 'subscription'){
-                    $subscription_exists = true;
-                }
-            }
-
-            if(! $subscription_exists)
-            {
+            if(! $this->subscriptionExists($subscription_id)) {
                 $rawResponse = $api->createSubscription($data);
                 $response = json_decode( $rawResponse, true );
 
@@ -351,6 +340,27 @@ abstract class eCurring_WC_Gateway_Abstract extends WC_Payment_Gateway
 
 		return array ( 'result' => 'failure' );
 	}
+
+    /**
+     * Check if subscription exists.
+     *
+     * @param $subscription_id
+     *
+     * @return bool
+     * @throws eCurring_WC_Exception_ApiClientException
+     */
+	protected function subscriptionExists($subscription_id)
+    {
+        if(! $subscription_id) {
+            return false;
+        }
+
+        $api = eCurring_WC_Plugin::getApiHelper();
+        $subscription_data = $api->getSubscriptionById($subscription_id);
+
+        return isset($subscription_data['data']['type']) && $subscription_data['data']['type'] === 'subscription';
+
+    }
 
 	protected function updateOrderWithSubscriptionData(array $response, WC_Order $order)
     {
