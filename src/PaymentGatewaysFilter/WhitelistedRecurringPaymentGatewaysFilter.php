26459b689780bc37482cd652234a59268d36b277
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2020-10-02 12:30:04 +0300

    add payment methods filter

---
+++ src/PaymentGatewaysFilter/WhitelistedRecurringPaymentGatewaysFilter.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace Ecurring\WooEcurring\PaymentGatewaysFilter;
+
+use WC_Payment_Gateway;
+
+/**
+ * Filter WooCommerce payment gateways, return only whitelisted and supporting 'subscription' ones.
+ */
+class WhitelistedRecurringPaymentGatewaysFilter implements PaymentGatewaysFilterInterface {
+
+	/**
+	 * @var string[] List of allowed payment gateway classes.
+	 */
+	protected $whiteList;
+
+	/**
+	 * @param string[] $whiteList White list of payment gateway class names.
+	 */
+	public function __construct(array $whiteList)
+	{
+		$this->whiteList = $whiteList;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function filter( array $gateways ): array {
+		return array_filter($gateways, function(WC_Payment_Gateway $gateway){
+			return $this->isGatewayClassWhitelisted($gateway) && $gateway->supports('subscription');
+		});
+	}
+
+	/**
+	 * Check if gateway is in the white list.
+	 *
+	 * @param WC_Payment_Gateway $gateway Gateway to check.
+	 *
+	 * @return bool
+	 */
+	protected function isGatewayClassWhitelisted( WC_Payment_Gateway $gateway ) {
+		return in_array(get_class($gateway), $this->whiteList, true);
+	}
+}
