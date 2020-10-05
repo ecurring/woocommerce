38eaba70d08b7ae88b631e5192f9eb97c3ff190a
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2020-10-05 13:02:50 +0300

    add test for payment complete listener `init()`

---
+++ tests/Unit/EventListener/PaymentCompleteEventListenerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace eCurring\WooEcurringTests\Unit\EventListener;
+
+
+use Ecurring\WooEcurring\Api\ApiClientInterface;
+use eCurring\WooEcurringTests\TestCase;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\MockObject\MockObject;
+use function Brain\Monkey\Functions\expect;
+use Ecurring\WooEcurring\EventListener\PaymentCompleteEventListener;
+
+class PaymentCompleteEventListenerTest extends TestCase {
+
+	use MockeryPHPUnitIntegration;	//to properly count Mockery expectations as assertions.
+
+	public function testInit(){
+
+		/** @var ApiClientInterface&MockObject $apiClientMock */
+		$apiClientMock = $this->createMock(ApiClientInterface::class);
+
+		$sut = new PaymentCompleteEventListener($apiClientMock);
+
+		expect('add_action')
+			->once()
+			->with(
+				'woocommerce_payment_complete',
+				[$sut, 'onPaymentComplete']
+			);
+
+		$sut->init();
+	}
+}
