46a467769013b717fb9b8c04e3730d634aae47ff
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2020-10-07 18:44:25 +0300

    introduce admin controller

---
+++ src/AdminPages/AdminController.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Handle admin pages requests.
+ */
+class AdminController {
+
+	private const PLUGIN_SETTINGS_TAB_SLUG = 'mollie_subscriptions';
+
+	/**
+	 * Initialize hooks listeners.
+	 */
+	public function init()
+	{
+		add_action('woocommerce_settings_tabs_array', [$this, 'registerPluginSettingsTab']);
+		add_action('woocommerce_settings_tabs' . self::PLUGIN_SETTINGS_TAB_SLUG, [$this, 'renderPluginSettingsPage']);
+	}
+
+	/**
+	 * Add plugin tab to the WC settings tabs.
+	 *
+	 * @param array $tabs Registered WC settings tabs.
+	 *
+	 * @return array WC settings tabs with this plugin tab added.
+	 */
+	public function registerPluginSettingsTab(array $tabs): array
+	{
+		$tabs[self::PLUGIN_SETTINGS_TAB_SLUG] = _x('Mollie Subscriptions', 'Plugin settings tab name', 'woo-ecurring');
+
+		return $tabs;
+	}
+
+	/**
+	 *
+	 */
+	public function renderPluginSettingsPage(): void
+	{
+		//todo
+	}
+
+
+}
