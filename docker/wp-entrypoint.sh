2f9970cc8aa0aa54e49db3eb09246a8e311c3097
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2020-10-20 18:29:44 +0300

    check if wp installed before install it

--- docker/wp-entrypoint.sh
+++ docker/wp-entrypoint.sh
@@ -3,6 +3,11 @@ set -e
 
 if wait-for-it.sh "${WORDPRESS_DB_HOST}" -t 60; then
   docker-entrypoint.sh apache2 -v
+
+  # WP_CLI is-* commands returns 0 if plugin is active, 1 otherwise
+  # https://developer.wordpress.org/cli/commands/plugin/is-active/
+  # https://developer.wordpress.org/cli/commands/core/is-installed/
+  wp core is-installed && \
   wp core install \
     --allow-root \
     --title="${WP_TITLE}" \
@@ -14,7 +19,6 @@ if wait-for-it.sh "${WORDPRESS_DB_HOST}" -t 60; then
   wp plugin is-installed akismet --allow-root && wp plugin uninstall akismet --allow-root --path="${DOCROOT_PATH}"
   wp plugin is-installed hello --allow-root && wp plugin uninstall hello --allow-root --path="${DOCROOT_PATH}"
 
-  # wp plugin is-active returns 0 if plugin is active, 1 otherwise https://developer.wordpress.org/cli/commands/plugin/is-active/
   wp plugin is-active "${PLUGIN_NAME}" && wp plugin activate "${PLUGIN_NAME}" --allow-root --path="${DOCROOT_PATH}"
 
 fi
