8568105026d065da2e33f0f8dffc87cfabd8e149
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2020-10-20 19:03:38 +0300

    fix checks for plugins and wp is installed

--- docker/wp-entrypoint.sh
+++ docker/wp-entrypoint.sh
@@ -7,7 +7,7 @@ if wait-for-it.sh "${WORDPRESS_DB_HOST}" -t 60; then
   # WP_CLI is-* commands returns 0 if plugin is active, 1 otherwise
   # https://developer.wordpress.org/cli/commands/plugin/is-active/
   # https://developer.wordpress.org/cli/commands/core/is-installed/
-  wp core is-installed && \
+  wp core is-installed --allow-root || \
   wp core install \
     --allow-root \
     --title="${WP_TITLE}" \
@@ -19,7 +19,7 @@ if wait-for-it.sh "${WORDPRESS_DB_HOST}" -t 60; then
   wp plugin is-installed akismet --allow-root && wp plugin uninstall akismet --allow-root --path="${DOCROOT_PATH}"
   wp plugin is-installed hello --allow-root && wp plugin uninstall hello --allow-root --path="${DOCROOT_PATH}"
 
-  wp plugin is-active "${PLUGIN_NAME}" && wp plugin activate "${PLUGIN_NAME}" --allow-root --path="${DOCROOT_PATH}"
+  wp plugin is-active --allow-root "${PLUGIN_NAME}" || wp plugin activate --allow-root "${PLUGIN_NAME}"  --path="${DOCROOT_PATH}"
 
 fi
 
