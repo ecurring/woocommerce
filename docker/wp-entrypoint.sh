527f837b6de8d11b5b653f82a6250afd5dedac94
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2020-10-20 18:14:36 +0300

    check if project plugin is active before activate
    In wp-entrypoint check if woo-ecurring plugin is active and activate it only if it's not active yet.
    
    Without this change every time on running this image WP_CLI tried to activate this plugin and produces unwanted output.

--- docker/wp-entrypoint.sh
+++ docker/wp-entrypoint.sh
@@ -13,9 +13,10 @@ if wait-for-it.sh "${WORDPRESS_DB_HOST}" -t 60; then
     --skip-email
   wp plugin is-installed akismet --allow-root && wp plugin uninstall akismet --allow-root --path="${DOCROOT_PATH}"
   wp plugin is-installed hello --allow-root && wp plugin uninstall hello --allow-root --path="${DOCROOT_PATH}"
-  wp plugin activate "${PLUGIN_NAME}" --allow-root --path="${DOCROOT_PATH}"
 
-  # Custom setup instructions
+  # wp plugin is-active returns 0 if plugin is active, 1 otherwise https://developer.wordpress.org/cli/commands/plugin/is-active/
+  wp plugin is-active "${PLUGIN_NAME}" && wp plugin activate "${PLUGIN_NAME}" --allow-root --path="${DOCROOT_PATH}"
+
 fi
 
 exec "$@"
