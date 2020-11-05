71cf4b162a6364244ac222f861cea8e6441ddc86
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2020-11-05 22:46:15 +0200

    add EnvironmentChecker test
    Add test case when woocommerce version is lower than required

--- tests/Unit/EnvironmentCheckerTest.php
+++ tests/Unit/EnvironmentCheckerTest.php
@@ -7,6 +7,7 @@ use Ecurring\WooEcurringTests\TestCase;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use function Brain\Monkey\Functions\expect;
 use function Brain\Monkey\Functions\when;
+use function Patchwork\redefine;
 
 class EnvironmentCheckerTest extends TestCase {
 
@@ -212,5 +213,77 @@ class EnvironmentCheckerTest extends TestCase {
         $this->assertTrue($stringFound, 'Not found expected message about WooCommerce not active.');
     }
 
+    public function testCheckEnvironmentCaseWoocommerceVersionTooLow()
+    {
+        $minRequiredPhpVersion = '7.2';
+        $minRequiredWcVersion = '4.0';
+        $actualPhpVersion = '7.2';
 
+        expect('phpversion')
+            ->andReturn($actualPhpVersion);
+
+        $sut = new EnvironmentChecker($minRequiredPhpVersion, $minRequiredWcVersion );
+
+        redefine('version_compare', function ($version1) use ($actualPhpVersion) {
+            //return true for PHP version check, false for WC version check
+            return $version1 === $actualPhpVersion;
+        });
+
+        expect('get_option')
+            ->with('active_plugins')
+            ->andReturn([]);
+
+        expect('apply_filters')
+            ->with('active_plugins', [])
+            ->andReturn(['woocommerce/woocommerce.php']);
+
+        when('admin_url')
+            ->justReturn('');
+
+        when('esc_url')
+            ->returnArg(1);
+
+        when('__')
+            ->returnArg(1);
+
+        when('esc_html__')
+            ->returnArg(1);
+
+        if(! defined('M4W_FILE')){
+            define('M4W_FILE', '');
+        }
+
+        $molliePluginBasename = 'mollie-for-woocommerce/mollie-for-woocommerce.php';
+
+        expect('plugin_basename')
+            ->with(M4W_FILE)
+            ->andReturn('woo-ecurring/woo-ecurring.php');
+
+        expect('is_plugin_active')
+            ->with($molliePluginBasename)
+            ->andReturn(true);
+
+        expect('get_plugin_data')
+            ->with(M4W_FILE)
+            ->andReturn(['Version' => '6.0.0']);
+
+        expect('extension_loaded')
+            ->with('json')
+            ->andReturn(true);
+
+        $this->assertFalse($sut->checkEnvironment(), 'EnvironmentChecker test false positive.');
+
+
+        $errors = $sut->getErrors();
+        $stringFound = false;
+
+        foreach ($errors as $errorMessage) {
+            if(stristr($errorMessage, 'update') && stristr($errorMessage, 'WooCommerce')){
+                $stringFound = true;
+                break;
+            }
+        }
+
+        $this->assertTrue($stringFound, 'Not found expected message about WooCommerce update required.');
+    }
 }
