07d676d53e84e2ff52641352b21cf7f1519ab1b3
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2020-11-12 17:05:58 +0200

    fix phpcs errors
    refactor Settings class to fix phpcs errors

--- src/Settings.php
+++ src/Settings.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Ecurring\WooEcurring;
 
 class Settings
@@ -8,49 +10,14 @@ class Settings
     {
         add_action(
             'admin_menu',
-            static function () {
+            function () {
                 add_menu_page(
                     'eCurring Settings',
                     'eCurring',
                     'administrator',
                     __FILE__,
-                    static function () { ?>
-                        <div class="wrap">
-                            <h1>eCurring Settings</h1>
-                            <form method="post" action="options.php">
-                                <?php settings_fields('ecurring-settings-group'); ?>
-                                <?php do_settings_sections('ecurring-settings-group'); ?>
-                                <table class="form-table">
-                                    <tbody>
-                                    <tr>
-                                        <th scope="row">Allow subscription options for customers</th>
-                                        <td><fieldset><legend class="screen-reader-text"><span>Allow subscription options for customers</span></legend>
-                                                <label for="ecurring_customer_subscription_pause">
-                                                    <input name="ecurring_customer_subscription_pause" type="checkbox" value="1"
-                                                        <?php checked(get_option('ecurring_customer_subscription_pause'), '1');?>>
-                                                    Pause Subscription
-                                                </label>
-                                                <br>
-                                                <label for="ecurring_customer_subscription_switch">
-                                                    <input name="ecurring_customer_subscription_switch" type="checkbox" value="1"
-                                                        <?php checked(get_option('ecurring_customer_subscription_switch'), '1');?>>
-                                                    Switch Subscription
-                                                </label>
-                                                <br>
-                                                <label for="ecurring_customer_subscription_cancel">
-                                                    <input name="ecurring_customer_subscription_cancel" type="checkbox" value="1"
-                                                        <?php checked(get_option('ecurring_customer_subscription_cancel'), '1');?>>
-                                                    Cancel Subscription
-                                                </label>
-                                            </fieldset></td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-
-                                <?php submit_button(); ?>
-                            </form>
-                        </div>
-                        <?php
+                    function () {
+                        echo wp_kses_post($this->renderSettingsPage());
                     }
                 );
             }
@@ -70,5 +37,56 @@ class Settings
                 );
             }
         );
+    }
+
+    /**
+     * Produce the Settings page HTML.
+     *
+     * @return string
+     */
+    protected function renderSettingsPage(): string
+    {
+        ob_start(); ?>
+        <div class="wrap">
+            <h1>eCurring Settings</h1>
+            <form method="post" action="options.php">
+                <?php settings_fields('ecurring-settings-group'); ?>
+                <?php do_settings_sections('ecurring-settings-group'); ?>
+                <table class="form-table">
+                    <tbody>
+                    <tr>
+                        <th scope="row">Allow subscription options for customers</th>
+                        <td>
+                            <fieldset>
+                                <legend class="screen-reader-text"><span>Allow subscription options for customers</span>
+                                </legend>
+                                <label for="ecurring_customer_subscription_pause">
+                                    <input name="ecurring_customer_subscription_pause" type="checkbox" value="1"
+                                        <?php checked(get_option('ecurring_customer_subscription_pause'), '1'); ?>>
+                                    Pause Subscription
+                                </label>
+                                <br>
+                                <label for="ecurring_customer_subscription_switch">
+                                    <input name="ecurring_customer_subscription_switch" type="checkbox" value="1"
+                                        <?php checked(get_option('ecurring_customer_subscription_switch'), '1'); ?>>
+                                    Switch Subscription
+                                </label>
+                                <br>
+                                <label for="ecurring_customer_subscription_cancel">
+                                    <input name="ecurring_customer_subscription_cancel" type="checkbox" value="1"
+                                        <?php checked(get_option('ecurring_customer_subscription_cancel'), '1'); ?>>
+                                    Cancel Subscription
+                                </label>
+                            </fieldset>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+
+                <?php submit_button(); ?>
+            </form>
+        </div>
+        <?php
+        return ob_get_clean();
     }
 }
