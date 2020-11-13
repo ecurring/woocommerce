7d4fccc8872918219055b07599d6cb351c50890a
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2020-11-13 16:43:17 +0200

    Revert "load `WordPress` in `psalm` bootstrap"
    This reverts commit fc18907d6c9a456a72d20452d61c6f253da59f80.

--- .psalm/autoloader.php
+++ .psalm/autoloader.php
@@ -48,7 +48,6 @@ require_once ABSPATH . WPINC . '/post-thumbnail-template.php';
 require_once ABSPATH . 'wp-admin/includes/plugin.php';
 
 require_once PROJECT_DIR . '/includes/ecurring/wc/plugin.php';
-require_once PROJECT_DIR . '/vendor/wordpress/wordpress/wp-load.php';
 
 require_once PROJECT_DIR . '/vendor/woocommerce/action-scheduler/functions.php';
 require_once PROJECT_DIR . '/vendor/woocommerce/woocommerce/includes/wc-order-functions.php';
