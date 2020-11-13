a9387bd6a0b3b9d98d131b9215f5db4dbff999d4
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2020-11-13 14:31:48 +0200

    refactoring

--- src/Assets.php
+++ src/Assets.php
@@ -23,9 +23,11 @@ class Assets
                     return;
                 }
 
+                $scriptFilePath = eCurring_WC_Plugin::getPluginUrl('assets/js/admin-subscriptions.js');
+
                 wp_enqueue_script(
                     'ecurring_admin_subscriptions',
-                    eCurring_WC_Plugin::getPluginUrl('assets/js/admin-subscriptions.js'),
+                    $scriptFilePath,
                     ['jquery'],
                     filemtime(
                         get_template_directory(
@@ -34,9 +36,11 @@ class Assets
                     )
                 );
 
+                $stylesFilePath = eCurring_WC_Plugin::getPluginUrl('assets/css/admin-subscriptions.css');
+
                 wp_enqueue_style(
                     'ecurring_admin_subscriptions',
-                    eCurring_WC_Plugin::getPluginUrl('assets/css/admin-subscriptions.css'),
+                    $stylesFilePath,
                     [],
                     filemtime(
                         get_template_directory(
@@ -53,9 +57,10 @@ class Assets
         add_action(
             'wp_enqueue_scripts',
             static function () {
+                $scriptFilePath = eCurring_WC_Plugin::getPluginUrl('assets/js/customer-subscriptions.js');
                 wp_enqueue_script(
                     'ecurring_customer_subscriptions',
-                    eCurring_WC_Plugin::getPluginUrl('assets/js/customer-subscriptions.js'),
+                    $scriptFilePath,
                     ['jquery'],
                     filemtime(
                         get_template_directory(
@@ -72,9 +77,11 @@ class Assets
                     ]
                 );
 
+                $stylesFilePath = eCurring_WC_Plugin::getPluginUrl('assets/css/customer-subscriptions.css');
+
                 wp_enqueue_style(
                     'ecurring_customer_subscriptions',
-                    eCurring_WC_Plugin::getPluginUrl('assets/css/customer-subscriptions.css'),
+                    $stylesFilePath,
                     [],
                     filemtime(
                         get_template_directory(
