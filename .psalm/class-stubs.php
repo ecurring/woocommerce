69a8dab502c25d5c70d6f5b452edc8df80a25045
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2020-11-13 18:29:34 +0200

    add `psalm` stubs for classes

---
+++ .psalm/class-stubs.php
@@ -0,0 +1,32 @@
+<?php
+
+class WC_Order
+{
+    public function get_id():int
+    {
+        return 0;
+    }
+
+    public function get_items(): array
+    {
+        return [
+            new WC_Order_Item_Product(),
+        ];
+    }
+}
+
+class WC_Order_Item_Product
+{
+    public function get_product(): WC_Product
+    {
+        return new WC_Product();
+    }
+}
+
+class WC_Product
+{
+}
+
+class WC_Order_Refund extends WC_Order
+{
+}
