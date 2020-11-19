bba35fc31f1ed4c9bf6603ae8af64301356cd17b
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2020-11-19 13:52:33 +0200

    fix phpcs errors

--- src/EnvironmentChecker/EnvironmentChecker.php
+++ src/EnvironmentChecker/EnvironmentChecker.php
@@ -72,8 +72,7 @@ class EnvironmentChecker implements EnvironmentCheckerInterface
     {
         $phpVersionIsOk = version_compare(phpversion(), $this->minPhpVersion, '>=');
 
-        if(! $phpVersionIsOk)
-        {
+        if (! $phpVersionIsOk) {
             $this->errors[] = __(
                 'Mollie Subscriptions plugin is disabled. Please, update your PHP first.',
                 'woo-ecurring'
@@ -92,7 +91,7 @@ class EnvironmentChecker implements EnvironmentCheckerInterface
     {
         $jsonExtensionLoaded = extension_loaded('json');
 
-        if(! $jsonExtensionLoaded){
+        if (! $jsonExtensionLoaded) {
             $this->errors[] = esc_html__(
                 'Mollie Subscriptions requires the JSON extension for PHP. Enable it in your server or ask your webhoster to enable it for you.',
                 'woo-ecurring'
@@ -111,11 +110,10 @@ class EnvironmentChecker implements EnvironmentCheckerInterface
     {
         $wcIsActive = in_array(
             'woocommerce/woocommerce.php',
-            apply_filters( 'active_plugins', get_option( 'active_plugins' ))
+            apply_filters('active_plugins', get_option('active_plugins'))
         );
 
-        if(! $wcIsActive){
-
+        if (! $wcIsActive) {
             $woocommercePluginPageUrl = $this->buildInstallPluginPageLink('woocommerce');
 
             $this->errors[] = sprintf(
@@ -138,20 +136,20 @@ class EnvironmentChecker implements EnvironmentCheckerInterface
      */
     protected function checkWoocommerceVersion(): bool
     {
-        if(! defined('WC_VERSION')){
+        if (! defined('WC_VERSION')) {
             return false;
         }
 
         $wcVersionIsOk = version_compare(WC_VERSION, $this->minWoocommerceVersion, '>=');
         $woocommercePluginPageUrl = $this->buildInstallPluginPageLink('woocommerce');
 
-        if(! $wcVersionIsOk) {
+        if (! $wcVersionIsOk) {
             $this->errors[] = sprintf(
             /* translators: %1$s is replaced with WooCommerce plugin installation page url. */
-            __(
-                '<strong>Mollie Subscriptions plugin is inactive.</strong> Please, update <a href="%1$s">WooCommerce</a> plugin first.',
-                'woo-ecurring'
-            ),
+                __(
+                    '<strong>Mollie Subscriptions plugin is inactive.</strong> Please, update <a href="%1$s">WooCommerce</a> plugin first.',
+                    'woo-ecurring'
+                ),
                 $woocommercePluginPageUrl
             );
         }
@@ -167,8 +165,7 @@ class EnvironmentChecker implements EnvironmentCheckerInterface
         $molliePluginBasename = $this->getMolliePluginBasename();
         $isMollieActive = is_plugin_active($molliePluginBasename);
 
-        if(! $isMollieActive){
-
+        if (! $isMollieActive) {
             $molliePluginPageUrl = $this->buildInstallPluginPageLink('mollie-payments-for-woocommerce');
 
             $this->errors[] = sprintf(
@@ -191,11 +188,11 @@ class EnvironmentChecker implements EnvironmentCheckerInterface
      */
     protected function getMolliePluginBasename(): ?string
     {
-        if(! defined('M4W_FILE')){
+        if (! defined('M4W_FILE')) {
             return null;
         }
 
-        if(! function_exists('plugin_basename')) {
+        if (! function_exists('plugin_basename')) {
             require_once ABSPATH . WPINC . '/plugin.php';
         }
 
@@ -209,9 +206,8 @@ class EnvironmentChecker implements EnvironmentCheckerInterface
     {
         $isMollieVersionOk = false;
 
-        if(defined('M4W_FILE')){
-
-            if(! function_exists('get_plugin_data')){
+        if (defined('M4W_FILE')) {
+            if (! function_exists('get_plugin_data')) {
                 require_once ABSPATH . 'wp-admin/includes/plugin.php';
             }
 
@@ -225,13 +221,12 @@ class EnvironmentChecker implements EnvironmentCheckerInterface
             );
         }
 
-        if(! $isMollieVersionOk){
-
+        if (! $isMollieVersionOk) {
             $molliePluginPageUrl = $this->buildInstallPluginPageLink('mollie-payments-for-woocommerce');
 
             $this->errors[] = $mollieIsNotMinimalVersionMessage = sprintf(
             /* translators: %1$s is replaced with Mollie plugin installation page url. */
-            __(
+                __(
                     '<strong>Mollie Subscriptions plugin is inactive.</strong> Please, update <a href="%1$s">Mollie Payments for WooCommerce</a> plugin first.',
                     'woo-ecurring'
                 ),
@@ -252,8 +247,8 @@ class EnvironmentChecker implements EnvironmentCheckerInterface
     protected function buildInstallPluginPageLink(string $pluginSlug): string
     {
         $pluginUrlQueryPart = http_build_query([
-            'tab'=> 'plugin-information',
-            'plugin' => $pluginSlug
+            'tab' => 'plugin-information',
+            'plugin' => $pluginSlug,
         ]);
 
         return admin_url('plugin-install.php?' . $pluginUrlQueryPart);
