cdbbb6a587c02642a1713eac66a04f516c35c164
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2020-11-23 20:39:55 +0200

    fix tests

--- tests/Unit/EventListener/MollieMandateCreatedEventListenerTest.php
+++ tests/Unit/EventListener/MollieMandateCreatedEventListenerTest.php
@@ -13,98 +13,105 @@ use WC_Order;
 use WC_Order_Item;
 use WC_Order_Item_Product;
 use WC_Product;
+
 use function Brain\Monkey\Functions\expect;
 use function Brain\Monkey\Functions\when;
 
-class MollieMandateCreatedEventListenerTest extends TestCase {
+class MollieMandateCreatedEventListenerTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
 
-	use MockeryPHPUnitIntegration;
+    public function testInit()
+    {
 
-	public function testInit(){
+        /** @var ApiClient&MockObject $apiClientMock */
+        $apiClientMock = $this->createMock(ApiClient::class);
 
-		/** @var ApiClient&MockObject $apiClientMock */
-		$apiClientMock = $this->createMock( ApiClient::class);
+        /** @var eCurring_WC_Helper_Data&MockObject $dataHelperMock */
+        $dataHelperMock = $this->createMock(eCurring_WC_Helper_Data::class);
 
-		/** @var eCurring_WC_Helper_Data&MockObject $dataHelperMock */
-		$dataHelperMock = $this->createMock(eCurring_WC_Helper_Data::class);
+        /** @var SubscriptionCrudInterface&MockObject $subscriptionCrudMock */
+        $subscriptionCrudMock = $this->createMock(SubscriptionCrudInterface::class);
 
-		/** @var SubscriptionCrudInterface&MockObject $subscriptionCrudMock */
-		$subscriptionCrudMock = $this->createMock(SubscriptionCrudInterface::class);
+        $sut = new MollieMandateCreatedEventListener($apiClientMock, $dataHelperMock, $subscriptionCrudMock);
 
-		$sut = new MollieMandateCreatedEventListener($apiClientMock, $dataHelperMock, $subscriptionCrudMock);
+        expect('add_action')
+        ->once()
+        ->with(
+            'mollie-payments-for-woocommerce_after_mandate_created',
+            [$sut, 'onMollieMandateCreated'],
+            10,
+            4
+        );
 
-		expect('add_action')
-		->once()
-		->with(
-			'mollie-payments-for-woocommerce_after_mandate_created',
-			[$sut, 'onMollieMandateCreated'],
-			10,
-			4
-		);
+        $sut->init();
+    }
 
-		$sut->init();
-	}
+    public function testOnMollieMandateCreated()
+    {
+        /** @var ApiClient&MockObject $apiClientMock */
+        $apiClientMock = $this->createMock(ApiClient::class);
 
-	public function testOnMollieMandateCreated()
-	{
-		/** @var ApiClient&MockObject $apiClientMock */
-		$apiClientMock = $this->createMock(ApiClient::class);
+        /** @var eCurring_WC_Helper_Data&MockObject $dataHelperMock */
+        $dataHelperMock = $this->createMock(eCurring_WC_Helper_Data::class);
 
-		/** @var eCurring_WC_Helper_Data&MockObject $dataHelperMock */
-		$dataHelperMock = $this->createMock(eCurring_WC_Helper_Data::class);
+        /** @var SubscriptionCrudInterface&MockObject $subscriptionCrudMock */
+        $subscriptionCrudMock = $this->createMock(SubscriptionCrudInterface::class);
 
-		/** @var SubscriptionCrudInterface&MockObject $subscriptionCrudMock */
-		$subscriptionCrudMock = $this->createMock(SubscriptionCrudInterface::class);
+        $sut = new MollieMandateCreatedEventListener($apiClientMock, $dataHelperMock, $subscriptionCrudMock);
+        $orderMock = $this->createMock(WC_Order::class);
 
-		$sut = new MollieMandateCreatedEventListener($apiClientMock, $dataHelperMock, $subscriptionCrudMock);
-		$orderMock = $this->createMock( WC_Order::class);
+        $orderItemProductMock = $this->createMock(WC_Order_Item_Product::class);
+        $orderItemMock = $this->createMock(WC_Order_Item::class);
 
-		$orderItemProductMock = $this->createMock( WC_Order_Item_Product::class);
-		$orderItemMock = $this->createMock( WC_Order_Item::class);
-		$orderMock->expects($this->once())
-			->method('get_items')
-			->willReturn(
-				[
-					$orderItemMock,
-					$orderItemProductMock
-				]
-			);
+        /** @var WC_Order&MockObject $orderMock */
+        $orderMock->expects($this->once())
+            ->method('get_items')
+            ->willReturn(
+                [
+                    $orderItemMock,
+                    $orderItemProductMock,
+                ]
+            );
+        $orderMock->expects($this->once())
+            ->method('get_billing_first_name')
+            ->willReturn('Name');
 
-		$productMock = $this->createMock( WC_Product::class);
+        $productMock = $this->createMock(WC_Product::class);
 
-		$ecurringSubscriptionId = 'ecurringsubscriptionid3463';
+        $ecurringSubscriptionId = 'ecurringsubscriptionid3463';
 
-		$subscriptionCrudMock->expects($this->once())
-			->method('getProductSubscriptionId')
-			->with($productMock)
-			->willReturn($ecurringSubscriptionId);
+        $subscriptionCrudMock->expects($this->once())
+            ->method('getProductSubscriptionId')
+            ->with($productMock)
+            ->willReturn($ecurringSubscriptionId);
 
-		$orderItemProductMock->expects($this->once())
-			->method('get_product')
-			->willReturn($productMock);
+        $orderItemProductMock->expects($this->once())
+            ->method('get_product')
+            ->willReturn($productMock);
 
-		$ecurringCustomerId = 'customerid324';
-		$dataHelperMock->expects($this->once())
-			->method('getUsereCurringCustomerId')
-			->willReturn($ecurringCustomerId);
+        $ecurringCustomerId = 'customerid324';
+        $dataHelperMock->expects($this->once())
+            ->method('getUsereCurringCustomerId')
+            ->willReturn($ecurringCustomerId);
 
-		when('add_query_arg')
-			->justReturn('');
+        when('add_query_arg')
+            ->justReturn('');
 
-		when('home_url')
-			->justReturn('');
+        when('home_url')
+            ->justReturn('');
 
-		when('get_user_meta')->justReturn(0);
+        when('get_user_meta')->justReturn(0);
 
-		$apiClientMock->expects($this->once())
-			->method('createSubscription')
-			->with(
-				$ecurringCustomerId,
-				$ecurringSubscriptionId,
-				''
-			)
-			->willReturn([]);
+        $apiClientMock->expects($this->once())
+            ->method('createSubscription')
+            ->with(
+                $ecurringCustomerId,
+                $ecurringSubscriptionId,
+                ''
+            )
+            ->willReturn([]);
 
-		$sut->onMollieMandateCreated(null, $orderMock, '', '');
-	}
+        $sut->onMollieMandateCreated(null, $orderMock, '', '');
+    }
 }
