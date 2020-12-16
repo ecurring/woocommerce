32c12e39282c5fed332dd251de5073ba38407c23
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2020-12-16 14:50:13 +0200

    import subscriptions if order exists only

--- src/Subscription/Repository.php
+++ src/Subscription/Repository.php
@@ -17,9 +17,8 @@ class Repository
      */
     public function createSubscriptions($subscriptions): void
     {
-
         foreach ($subscriptions->data as $subscription) {
-            if (! $this->subscriptionExistsInDb($subscription->id)) {
+            if (! $this->subscriptionExistsInDb($subscription->id) && $this->orderWithSubscriptionExists($subscription->id)) {
                 $this->create($subscription);
             }
         }
@@ -121,6 +120,60 @@ class Repository
         );
 
         return $found[0] ?? 0;
+    }
+
+    /**
+     * Check if order with the given subscription id exists.
+     *
+     * @param string $subscriptionId The id of the subscription to look for.
+     *
+     * @return bool True if order containing given subscription id exists, false otherwise.
+     */
+    protected function orderWithSubscriptionExists(string $subscriptionId): bool
+    {
+        $foundOrderId = $this->findSubscriptionOrderIdBySubscriptionId($subscriptionId);
+
+        return $foundOrderId !== 0;
+    }
+
+    /**
+     * Return an id of the order containing given subscription, return 0 if not found.
+     *
+     * @param string $subscriptionId Subscription id to find order with.
+     *
+     * @return int Found order id.
+     */
+    protected function findSubscriptionOrderIdBySubscriptionId(string $subscriptionId): int
+    {
+        $addSubscriptionIdMetaSupport = function (array $wpQueryArgs, array $wcOrdersQueryArgs) use ($subscriptionId): array {
+            if (! empty($wcOrdersQueryArgs['_ecurring_subscription_id'])) {
+                $wpQueryArgs['meta_query'][] = [
+                    'key' => '_ecurring_subscription_id',
+                    'value' => $subscriptionId,
+                ];
+            }
+
+            return $wpQueryArgs;
+        };
+
+        add_filter(
+            'woocommerce_order_data_store_cpt_get_orders_query',
+            $addSubscriptionIdMetaSupport
+        );
+
+        $foundIds = wc_get_orders(
+            [
+                'limit' => 1,
+                'return' => 'ids',
+            ]
+        );
+
+        remove_filter(
+            'woocommerce_order_data_store_cpt_get_orders_query',
+            $addSubscriptionIdMetaSupport
+        );
+
+        return $foundIds[0] ?? 0;
     }
 
     /**
