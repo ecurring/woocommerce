edbdadffde3caa092d8df86b1f804f7fd1321d1d
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2020-12-23 17:28:02 +0200

    add mandate factory interface

--- src/Subscription/Mandate/SubscriptionMandateFactoryInterface.php
+++ src/Subscription/Mandate/SubscriptionMandateFactoryInterface.php
@@ -14,7 +14,8 @@ interface SubscriptionMandateFactoryInterface
     /**
      * @param string $code Mandate code.
      * @param string $confirmationPageUrl URL of the page where mandate can be accepted.
-     * @param bool $confirmationSent Whether confirmation with confirmation page url was sent to the customer.
+     * @param bool $confirmationSent Whether confirmation with
+     *                               confirmation page url was sent to the customer.
      * @param bool $accepted Whether mandate was accepted.
      * @param DateTime|null $acceptedDate The date when mandate was accepted or null if it wasn't.
      *
