b9a6aab225ff58fb47e39ede91fd3eb2629bd1c9
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2021-01-04 18:49:59 +0200

    Add subscription factory interface and exception

---
+++ src/Subscription/SubscriptionFactory/DataBasedSubscriptionFactoryInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ecurring\WooEcurring\Subscription\SubscriptionFactory;
+
+use ArrayAccess;
+use Ecurring\WooEcurring\Subscription\SubscriptionInterface;
+
+/**
+ * Service able to create a subscription instance from data.
+ */
+interface DataBasedSubscriptionFactoryInterface
+{
+    /**
+     * Create a new subscription from given data array.
+     *
+     * @param array|ArrayAccess $subscriptionData
+     *
+     * @return SubscriptionInterface Created subscription instance.
+     *
+     * @throws SubscriptionFactoryException If cannot create a new subscription instance.
+     */
+    public function createSubscription($subscriptionData): SubscriptionInterface;
+}
---
+++ src/Subscription/SubscriptionFactory/SubscriptionFactoryException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ecurring\WooEcurring\Subscription\SubscriptionFactory;
+
+use Ecurring\WooEcurring\EcurringException;
+
+/**
+ * Should be thrown if error occurred when trying to create a subscription instance.
+ */
+class SubscriptionFactoryException extends EcurringException
+{
+
+}
