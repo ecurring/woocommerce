2566777d343546712371ad20d261f1f6541da742
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2021-01-05 12:46:45 +0200

    normalize subscription data

--- src/Subscription/SubscriptionFactory/DataBasedSubscriptionFactory.php
+++ src/Subscription/SubscriptionFactory/DataBasedSubscriptionFactory.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Ecurring\WooEcurring\Subscription\SubscriptionFactory;
 
 use ArrayAccess;
+use DateTime;
 use Ecurring\WooEcurring\Subscription\Mandate\SubscriptionMandateFactoryInterface;
 use Ecurring\WooEcurring\Subscription\Status\SubscriptionStatusFactoryInterface;
 use Ecurring\WooEcurring\Subscription\Subscription;
 use Ecurring\WooEcurring\Subscription\SubscriptionInterface;
+use Exception;
 
 /**
  * Service for creating subscription instances using mandate and status factories.
@@ -76,9 +78,54 @@ class DataBasedSubscriptionFactory implements DataBasedSubscriptionFactoryInterf
      * @param array|ArrayAccess $subscriptionData Subscription data that need to be normalized.
      *
      * @return array
+     * @throws SubscriptionFactoryException
      */
     protected function normalizeSubscriptionData($subscriptionData): array
     {
-        return $subscriptionData;
+        $subscriptionData = (array) $subscriptionData;
+
+        return [
+            'mandate_code' => $subscriptionData['mandate_code'] ?? '',
+            'confirmation_page' => $subscriptionData['confirmation_page'] ?? '',
+            'confirmation_sent' => $subscriptionData['confirmation_sent'] ?? false,
+            'mandate_accepted' => $subscriptionData['mandate_accepted'] ?? false,
+            'mandate_accepted_date' => $this->createDateFromArrayField(
+                $subscriptionData,
+                'mandate_accepted_date'
+            ),
+            'status' => $subscriptionData['status'] ?? '',
+            'start_date' => $this->createDateFromArrayField($subscriptionData, 'start_date'),
+            'cancel_date' => $this->createDateFromArrayField($subscriptionData, 'cancel_date'),
+            'resume_date' => $this->createDateFromArrayField($subscriptionData, 'resume_date'),
+            'created_at' => $this->createDateFromArrayField($subscriptionData, 'created_at'),
+            'updated_at' => $this->createDateFromArrayField($subscriptionData, 'updated_at'),
+            'archived' => $subscriptionData['archived'] ?? false,
+        ];
+    }
+
+    /**
+     * @param array $subscriptionDataArray
+     * @param string $dateFieldName
+     *
+     * @return DateTime|null Created object or null if field not set or equals null.
+     *
+     * @throws SubscriptionFactoryException If cannot create date from array field.
+     */
+    protected function createDateFromArrayField(array $subscriptionDataArray, string $dateFieldName): ?DateTime
+    {
+        try {
+            $date = $subscriptionDataArray[$dateFieldName] ?
+                new DateTime($subscriptionDataArray[$dateFieldName]) :
+                null;
+        } catch (Exception $exception) {
+            throw new SubscriptionFactoryException(
+                sprintf(
+                    'Couldn\'t parse date in subscription data. Exception caught when trying to create a DateTime object: %1$s',
+                    $exception->getMessage()
+                )
+            );
+        }
+
+        return $date;
     }
 }
