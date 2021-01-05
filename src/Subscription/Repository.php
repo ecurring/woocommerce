9674327e68c6963a18956fcc391a251828337b48
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2021-01-05 18:26:20 +0200

    save subscription data in meta fields

--- src/Subscription/Repository.php
+++ src/Subscription/Repository.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Ecurring\WooEcurring\Subscription;
 
 use Ecurring\WooEcurring\Api\Customers;
+use Ecurring\WooEcurring\Subscription\Mandate\SubscriptionMandateInterface;
+use Ecurring\WooEcurring\Subscription\Status\SubscriptionStatusInterface;
 use eCurring_WC_Helper_Api;
 use eCurring_WC_Helper_Settings;
 use eCurring_WC_Plugin;
@@ -39,12 +41,12 @@ class Repository
         $this->persistSubscription($subscription);
     }
 
-    protected function persistSubscription($subscription): void
+    protected function persistSubscription(SubscriptionInterface $subscription): void
     {
         $postId = wp_insert_post(
             [
                 'post_type' => 'esubscriptions',
-                'post_title' => $subscription->id,
+                'post_title' => $subscription->getId(),
                 'post_status' => 'publish',
             ]
         );
@@ -52,7 +54,7 @@ class Repository
         if ($postId && is_int($postId)) {
             $customer = $this->getCustomerApi();
             $customerDetails = $customer->getCustomerById(
-                $subscription->relationships->customer->data->id
+                $subscription->getCustomerId()
             );
 
             $this->saveSubscriptionData($postId, $subscription, $customerDetails);
@@ -60,7 +62,7 @@ class Repository
             eCurring_WC_Plugin::debug(
                 sprintf(
                     'Subscription %1$s successfully saved as post %2$d',
-                    $subscription->id,
+                    $subscription->getId(),
                     $postId
                 )
             );
@@ -77,28 +79,29 @@ class Repository
         $this->saveSubscriptionData($subscriptionPostId, $subscription);
     }
 
-    protected function saveSubscriptionData(int $subscriptionPostId, $subscriptionData, $customerDetails = null): void
-    {
+    protected function saveSubscriptionData(
+        int $subscriptionPostId,
+        SubscriptionInterface $subscription,
+        $customerDetails = null
+    ): void {
         update_post_meta(
             $subscriptionPostId,
             '_ecurring_post_subscription_id',
-            $subscriptionData->id
+            $subscription->getId()
         );
         update_post_meta(
             $subscriptionPostId,
-            '_ecurring_post_subscription_links',
-            $subscriptionData->links
+            '_ecurring_post_subscription_links', //todo: save subscription url instead or build it using subscription id.
+            []
         );
         update_post_meta(
             $subscriptionPostId,
-            '_ecurring_post_subscription_attributes',
-            $subscriptionData->attributes
+            '_ecurring_post_subscription_customer_id',
+            $subscription->getCustomerId()
         );
-        update_post_meta(
-            $subscriptionPostId,
-            '_ecurring_post_subscription_relationships',
-            $subscriptionData->relationships
-        );
+
+        $this->saveMandateFields($subscriptionPostId, $subscription->getMandate());
+        $this->saveSubscriptionStatusFields($subscriptionPostId, $subscription->getStatus());
 
         if ($customerDetails !== null) {
             update_post_meta(
@@ -107,6 +110,97 @@ class Repository
                 $customerDetails
             );
         }
+    }
+
+    /**
+     * @param int $subscriptionPostId
+     * @param SubscriptionMandateInterface $mandate
+     */
+    protected function saveMandateFields(int $subscriptionPostId, SubscriptionMandateInterface $mandate): void
+    {
+        update_post_meta(
+            $subscriptionPostId,
+            '_ecurring_post_subscription_mandate_accepted',
+            $mandate->getAccepted()
+        );
+
+        $mandateAcceptedDate = $mandate->getAcceptedDate();
+        update_post_meta(
+            $subscriptionPostId,
+            '_ecurring_post_subscription_mandate_accepted_date',
+            $mandateAcceptedDate ? $mandateAcceptedDate->format('c') : ''
+        );
+
+        update_post_meta(
+            $subscriptionPostId,
+            '_ecurring_post_subscription_mandate_confirmation_page',
+            $mandate->getConfirmationPageUrl()
+        );
+
+        update_post_meta(
+            $subscriptionPostId,
+            '_ecurring_post_subscription_mandate_confirmation_sent',
+            $mandate->getConfirmationSent()
+        );
+
+        update_post_meta(
+            $subscriptionPostId,
+            '_ecurring_post_subscription_mandate_code',
+            $mandate->getMandateCode()
+        );
+    }
+
+    protected function saveSubscriptionStatusFields(
+        int $subscriptionPostId,
+        SubscriptionStatusInterface $subscriptionStatus
+    ): void {
+        update_post_meta(
+            $subscriptionPostId,
+            '_ecurring_post_subscription_status',
+            $subscriptionStatus->getCurrentStatus()
+        );
+
+        $startDate = $subscriptionStatus->getStartDate();
+
+        update_post_meta(
+            $subscriptionPostId,
+            '_ecurring_post_subscription_start_date',
+            $startDate ? $startDate->format('c') : ''
+        );
+
+        $cancelDate = $subscriptionStatus->getCancelDate();
+        update_post_meta(
+            $subscriptionPostId,
+            '_ecurring_post_subscription_cancel_date',
+            $cancelDate ? $cancelDate->format('c') : ''
+        );
+
+        $resumeDate = $subscriptionStatus->getResumeDate();
+        update_post_meta(
+            $subscriptionPostId,
+            '_ecurring_post_subscription_resume_date',
+            $resumeDate ? $resumeDate->format('c') : ''
+        );
+
+        $createdAt = $subscriptionStatus->getCreatedAt();
+        update_post_meta(
+            $subscriptionPostId,
+            '_ecurring_post_subscription_created_at',
+            $createdAt ? $createdAt->format('c') : ''
+        );
+
+        $updatedAt = $subscriptionStatus->getUpdatedAt();
+        update_post_meta(
+            $subscriptionPostId,
+            '_ecurring_post_subscription_updated_at',
+            $updatedAt ? $updatedAt->format('c') : ''
+        );
+
+        update_post_meta(
+            $subscriptionPostId,
+            '_ecurring_post_subscription_archived',
+            $subscriptionStatus->getArchived()
+        );
     }
 
     /**
@@ -168,7 +262,7 @@ class Repository
      */
     protected function findSubscriptionOrderIdBySubscriptionId(string $subscriptionId): int
     {
-        $addSubscriptionIdMetaSupport = function (array $wpQueryArgs, array $wcOrdersQueryArgs) use ($subscriptionId): array {
+        $addSubscriptionIdMetaSupport = static function (array $wpQueryArgs, array $wcOrdersQueryArgs) use ($subscriptionId): array {
             if (! empty($wcOrdersQueryArgs['_ecurring_subscription_id'])) {
                 $wpQueryArgs['meta_query'][] = [
                     'key' => '_ecurring_subscription_id',
