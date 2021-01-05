ede991854ec83ff9d1973bef0e40fa5e0cb15035
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2021-01-05 16:42:50 +0200

    update subscription factory interface

--- src/Subscription/SubscriptionFactory/DataBasedSubscriptionFactoryInterface.php
+++ src/Subscription/SubscriptionFactory/DataBasedSubscriptionFactoryInterface.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Ecurring\WooEcurring\Subscription\SubscriptionFactory;
 
-use ArrayAccess;
 use Ecurring\WooEcurring\Subscription\SubscriptionInterface;
 
 /**
@@ -15,12 +14,12 @@ interface DataBasedSubscriptionFactoryInterface
     /**
      * Create a new subscription from given data array.
      *
-     * @param string $subscriptionId Subscription id in the eCurring system.
-     * @param array|ArrayAccess $subscriptionData Subscription attributes formatted as map.
+     * @param array $subscriptionData Subscription data formatted
+     *                                  as {@link https://docs.ecurring.com/subscriptions/get/ eCurring subscription}
      *
      * @return SubscriptionInterface Created subscription instance.
      *
      * @throws SubscriptionFactoryException If cannot create a new subscription instance.
      */
-    public function createSubscription(string $subscriptionId, $subscriptionData): SubscriptionInterface;
+    public function createSubscription($subscriptionData): SubscriptionInterface;
 }
