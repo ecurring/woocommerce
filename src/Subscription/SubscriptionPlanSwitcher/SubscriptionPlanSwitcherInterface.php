5b3c64b733a98cb37c19f2a6bb64f5978afc205a
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2021-01-11 11:42:42 +0200

    add subscription plan switcher interface

---
+++ src/Subscription/SubscriptionPlanSwitcher/SubscriptionPlanSwitcherInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ecurring\WooEcurring\Subscription\SubscriptionPlanSwitcher;
+
+use Ecurring\WooEcurring\Subscription\SubscriptionInterface;
+
+/**
+ * Service able to switch subscription plan using given subscription instance.
+ */
+interface SubscriptionPlanSwitcherInterface
+{
+
+    /**
+     * Switch subscription plan on the remote API and return a new subscription instance.
+     *
+     * @param SubscriptionInterface $oldSubscriptionInstance Subscription instance with
+     *          current subscription plan.
+     * @param string $newPlanId Id of the subscription plan to switch to.
+     *
+     * @return SubscriptionInterface A new subscription instance with given subscription plan.
+     */
+    public function switchSubscriptionPlan(
+        SubscriptionInterface $oldSubscriptionInstance,
+        string $newPlanId
+    ): SubscriptionInterface;
+}
