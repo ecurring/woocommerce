63d9657b40955eb67136bf8dc8a69af377803872
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2021-01-11 16:04:53 +0200

    allow subscription switch at certain date

--- src/Subscription/SubscriptionPlanSwitcher/SubscriptionPlanSwitcherInterface.php
+++ src/Subscription/SubscriptionPlanSwitcher/SubscriptionPlanSwitcherInterface.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Ecurring\WooEcurring\Subscription\SubscriptionPlanSwitcher;
 
+use DateTime;
 use Ecurring\WooEcurring\Subscription\SubscriptionInterface;
 
 /**
@@ -18,11 +19,13 @@ interface SubscriptionPlanSwitcherInterface
      * @param SubscriptionInterface $oldSubscription Subscription instance with
      *          current subscription plan.
      * @param string $newPlanId Id of the subscription plan to switch to.
+     * @param DateTime $switchDate The date when the switch should happen.
      *
      * @return SubscriptionInterface A new subscription instance with given subscription plan.
      */
     public function switchSubscriptionPlan(
         SubscriptionInterface $oldSubscription,
-        string $newPlanId
+        string $newPlanId,
+        DateTime $switchDate
     ): SubscriptionInterface;
 }
