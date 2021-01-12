7eaca222b4bd8c2978a090b9c6c495ab2aeedd6d
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2021-01-12 18:15:03 +0200

    remove not used function import

--- src/Customer/Subscriptions.php
+++ src/Customer/Subscriptions.php
@@ -10,7 +10,6 @@ use Ecurring\WooEcurring\Api\SubscriptionPlans;
 
 use function get_user_meta;
 use function get_current_user_id;
-use function json_decode;
 use function esc_attr;
 use function ucfirst;
 use function get_option;
