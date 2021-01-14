6dcef944e549d7eb5e50833e48e3747f22daecff
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2021-01-14 19:17:50 +0200

    add test for Repository#insert()

--- tests/Unit/Subscription/RepositoryTest.php
+++ tests/Unit/Subscription/RepositoryTest.php
@@ -2,114 +2,60 @@
 
 namespace Ecurring\WooEcurringTests\Unit\Subscription;
 
+use Ecurring\WooEcurring\Api\Customers;
+use Ecurring\WooEcurring\Subscription\Repository;
+use Ecurring\WooEcurring\Subscription\SubscriptionFactory\DataBasedSubscriptionFactoryInterface;
+use Ecurring\WooEcurring\Subscription\SubscriptionInterface;
 use Ecurring\WooEcurringTests\TestCase;
+use Mockery;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 
+use function Brain\Monkey\Functions\expect;
 
 class RepositoryTest extends TestCase
 {
     use MockeryPHPUnitIntegration;
 
-    public function subscriptionsDataProvider(): array
+    public function testInsert()
     {
+        $pluginMock = Mockery::mock('alias:eCurring_WC_Plugin');
+        $pluginMock->shouldReceive('debug')
+            ->with(Mockery::on(function ($message) {
+                return (bool) stristr($message, 'successfully saved as post');
+            }));
 
-        $subscription1 = (object) [
-            'links' => [
-                'self' => '',
-            ],
-                'type' => 'subscription',
-                'id' => 'subscriptionid123',
-                'links' => [
-                    'self' => '',
-                ],
-                'attributes' => [
-                    'mandate_code' => '',
-                    'mandate_accepted' => true,
-                ],
-                'relationships' => [
-                    'subscriptions-plan' => [
-                        'data' => [
-                            'type' => 'subscription-plan',
-                            'id' => 1,
-                        ],
-                    ],
-                ],
-                'customer' => [
-                    'data' => [
-                        'type' => 'customer',
-                        'id' => 2,
-                    ],
-                ],
-                'transactions' => [
-                    'links' => [
-                        'related' => '',
-                    ],
-                    'data' => [
-                        'type' => 'transaction',
-                        'id' => '12345-6789-1203-2342',
-                    ],
-                ],
-        ];
-
-        $subscription2 = (object) [
-            'links' => [
-                'self' => '',
-            ],
-                'type' => 'subscription',
-                'id' => 'subscriptionid456',
-                'links' => [
-                    'self' => '',
-                ],
-                'attributes' => [
-                    'mandate_code' => '',
-                    'mandate_accepted' => true,
-                ],
-                'relationships' => [
-                    'subscriptions-plan' => [
-                        'data' => [
-                            'type' => 'subscription-plan',
-                            'id' => 2,
-                        ],
-                    ],
-                ],
-                'customer' => [
-                    'data' => [
-                        'type' => 'customer',
-                        'id' => 3,
-                    ],
-                ],
-                'transactions' => [
-                    'links' => [
-                        'related' => '',
-                    ],
-                    'data' => [
-                        'type' => 'transaction',
-                        'id' => '9876-5432-1293-8573',
-                    ],
-                ],
-        ];
-
-        $subscriptionsSet = (object) [
-            'meta' => [
-                    'total' => 2,
-                ],
-            'links' => [
-                'self' => '',
-                'first' => '',
-                'last' => '',
-                'prev' => null,
-                'next' => null,
-            ],
-            'data' => [
-                $subscription1,
-                $subscription2,
-            ],
-        ];
-
-        return [
+        $orderId = 2432;
+        $subscriptionId = 5435;
+        $insertedPostId = 213;
+        $ecurringCustomerId = 8453;
+        $customerDetails = [];
+        $subscriptionMock = $this->createConfiguredMock(
+            SubscriptionInterface::class,
             [
-                $subscriptionsSet,
-            ],
-        ];
+                'getId' => $subscriptionId,
+                'getCustomerId' => $ecurringCustomerId,
+            ]
+        );
+
+        expect('wp_insert_post')
+            ->with(
+                [
+                'post_type' => 'esubscriptions',
+                'post_title' => $subscriptionId,
+                'post_status' => 'publish',
+                ]
+            )->andReturn($insertedPostId);
+
+        $subscriptionFactoryMock = $this->createMock(DataBasedSubscriptionFactoryInterface::class);
+        $customersApiClientMock = $this->createMock(Customers::class);
+        $customersApiClientMock->expects($this->once())
+            ->method('getCustomerById')
+            ->with($ecurringCustomerId)
+            ->willReturn($customerDetails);
+
+        expect('update_post_meta');
+
+        $sut = new Repository($subscriptionFactoryMock, $customersApiClientMock);
+        $sut->insert($subscriptionMock, $orderId);
     }
 }
