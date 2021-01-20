72c13ac37e9502836e9172724d9f7cad4e721c28
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2021-01-20 15:18:30 +0200

    Revert "make docker environment use PHP 8"
    This reverts commit 9b172d033756ac7c106b4536db10176a91e8310a.

--- docker/wp-entrypoint.sh
+++ docker/wp-entrypoint.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 set -e
 
-#if wait-for-it.sh "${WORDPRESS_DB_HOST}" -t 60; then
-#  docker-entrypoint.sh apache2 -v
-#
-#  wp core is-installed --allow-root || \
-#  wp core install \
-#    --allow-root \
-#    --title="${WP_TITLE}" \
-#    --admin_user="${ADMIN_USER}" \
-#    --admin_password="${ADMIN_PASS}" \
-#    --url="${WP_DOMAIN}" \
-#    --admin_email="${ADMIN_EMAIL}" \
-#    --skip-email
-#  wp plugin is-installed akismet --allow-root && wp plugin uninstall akismet --allow-root --path="${DOCROOT_PATH}"
-#  wp plugin is-installed hello --allow-root && wp plugin uninstall hello --allow-root --path="${DOCROOT_PATH}"
-#
-#  wp plugin is-active --allow-root "${PLUGIN_NAME}" || wp plugin activate --allow-root "${PLUGIN_NAME}"  --path="${DOCROOT_PATH}"
-#
-#fi
+if wait-for-it.sh "${WORDPRESS_DB_HOST}" -t 60; then
+  docker-entrypoint.sh apache2 -v
+
+  wp core is-installed --allow-root || \
+  wp core install \
+    --allow-root \
+    --title="${WP_TITLE}" \
+    --admin_user="${ADMIN_USER}" \
+    --admin_password="${ADMIN_PASS}" \
+    --url="${WP_DOMAIN}" \
+    --admin_email="${ADMIN_EMAIL}" \
+    --skip-email
+  wp plugin is-installed akismet --allow-root && wp plugin uninstall akismet --allow-root --path="${DOCROOT_PATH}"
+  wp plugin is-installed hello --allow-root && wp plugin uninstall hello --allow-root --path="${DOCROOT_PATH}"
+
+  wp plugin is-active --allow-root "${PLUGIN_NAME}" || wp plugin activate --allow-root "${PLUGIN_NAME}"  --path="${DOCROOT_PATH}"
+
+fi
 
 exec "$@"
