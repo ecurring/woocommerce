868274e47bffe8b884e6d3e533050573e0b5bcdf
Kirill Braslavsky <k.braslavskyi@inpsyde.com>
2021-01-28 16:46:16 +0200

    Revert "add messages for subscription post updated"
    This reverts commit 44427a122e6d31044a3aaa834f1883dfdd32351c.

--- src/Subscription/PostType.php
+++ src/Subscription/PostType.php
@@ -7,7 +7,6 @@ namespace Ecurring\WooEcurring\Subscription;
 use DateTime;
 use Ecurring\WooEcurring\Api\SubscriptionPlans;
 use eCurring_WC_Helper_Api;
-use WP_Post;
 
 class PostType
 {
@@ -40,54 +39,6 @@ class PostType
             10,
             1
         );
-
-        add_filter('post_updated_messages', [$this, 'setPostUpdatedMessages']);
-    }
-
-    /**
-     * Configure messages displayed when post of this type was updated.
-     *
-     * @param array $messages
-     *
-     * @see https://developer.wordpress.org/reference/functions/register_post_type/#comment-352
-     *
-     * @return array
-     */
-    public function setPostUpdatedMessages($messages): array
-    {
-        $post = get_post();
-
-        if (! $post instanceof WP_Post) {
-            return $messages;
-        }
-
-        $revision = filter_input(INPUT_GET, 'revision', FILTER_SANITIZE_NUMBER_INT);
-
-        $messages['esubscriptions'] = [
-            0 => '', // Unused. Messages start at index 1.
-            1 => __('Subscription updated.', 'woo-ecurring'),
-            2 => __('Custom field updated.', 'woo-ecurring'),
-            3 => __('Custom field deleted.', 'woo-ecurring'),
-            4 => __('Subscription updated.', 'woo-ecurring'),
-            5 => $revision ?
-                sprintf(
-                    /* translators: %s: date and time of the revision */
-                    __('Subscription restored to revision from %1$s', 'woo-ecurring'),
-                    wp_post_revision_title($revision, false)
-                ) :
-                false,
-            6 => __('Subscription published.', 'woo-ecurring'),
-            7 => __('Subscription saved.', 'woo-ecurring'),
-            8 => __('Subscription submitted.', 'woo-ecurring'),
-            9 => sprintf(
-                /* translators: Publish box date format, see http://php.net/date */
-                __('Subscription scheduled for: <strong>%1$s</strong>.', 'woo-ecurring'),
-                date_i18n(__('M j, Y @ G:i', 'woo-ecurring'), strtotime($post->post_date))
-            ),
-            10 => __('Subscription draft updated.', 'textdomain'),
-        ];
-
-        return $messages;
     }
 
     protected function register(): void
@@ -101,7 +52,7 @@ class PostType
                         'name' => esc_html__('Subscriptions', 'woo-ecurring'),
                         'singular_name' => esc_html__('Subscription', 'woo-ecurring'),
                         'menu_name' => esc_html__('Subscriptions', 'woo-ecurring'),
-                        'add_new_item' => __('Add new Subscription', 'woo-ecurring'),
+                        'add_new_item' => __('Add New Subscription', 'woo-ecurring'),
                         'new_item' => __('New Subscription', 'woo-ecurring'),
                         'edit_item' => __('Edit Subscription', 'woo-ecurring'),
                         'view_item' => __('View Subscription', 'woo-ecurring'),
